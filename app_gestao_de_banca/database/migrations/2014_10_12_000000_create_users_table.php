6bf07ff610f7e9f4ea00bbafb74ee6ce823509b9
AwadaAdel <88447929+AwadaAdel@users.noreply.github.com>
2021-12-22 19:11:39 -0300

    Update 2014_10_12_000000_create_users_table.php

--- app_gestao_de_banca/database/migrations/2014_10_12_000000_create_users_table.php
+++ app_gestao_de_banca/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('db_usuarios', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
+            $table->date('created_at');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
